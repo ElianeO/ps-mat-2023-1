d3485d2b3119a2dbbf3d6bcc2f5bda996caa787e
Eliane Cristina <89078266+ElianeO@users.noreply.github.com>
2023-04-27 12:05:05 +0000

    (25/04) Finalização da página de login

--- back-end/app.js
+++ back-end/app.js
@@ -1,26 +1,31 @@
-//Carrega as variáveis de ambiente do arquivo .env para a aplicação
-require ('dotenv').config()
+// Carrega as variáveis de ambiente do arquivo
+// .env para a aplicação
+require('dotenv').config()
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
-//Conexão ao BD
+// Habilita que qualquer origem de front-end possa
+// acessar o back-end
+const cors = require('cors')
+app.use(cors())
+
+// Conexão ao BD ------------------------------------------
 const db = require('./models')
 
 try {
-db.sequelize.authenticate()
-console.log('SEQUELIZE: connection has been established successfully')
+  db.sequelize.authenticate()
+  console.log('SEQUELIZE: connection has been established successfully.')
 }
-catch(error){
-console.error('*SEQUELIZE: unable to connect to the database: ', error)
-process.exit(1)
+catch(error) {
+  console.error('* SEQUELIZE: unable to connect to the database: ', error)
+  process.exit(1)     // Encerra o servidor com erro
 }
+// ---------------------------------------------------------
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -28,9 +33,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+// Chama a verificação de autenticação para qualquer rota
+const auth = require('./lib/auth')
+app.use(auth)
 /******************** ROTAS ******************************************/
 const users = require('./routes/users')
 app.use('/users', users)
